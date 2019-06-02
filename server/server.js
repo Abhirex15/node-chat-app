f2d66daa8883473784a0f5b7e3a40a9184b59c7c
Abhishek <abhirex160@gmail.com>
2019-06-02 16:55:33 +0530

    Changes made finally for a basic idea of chat codes

--- server/server.js
+++ server/server.js
@@ -30,10 +30,10 @@ socket.broadcast.emit('newMessage', {
   createAt: new Date().getTime()
 });
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
    io.emit('newMessage', generateMessage(message.from, message.text));
-
+      callback('This is from the server');
       // socket.broadcast.emit('newMessage', {
       //   from:message.from,
       //   text:message.text,
